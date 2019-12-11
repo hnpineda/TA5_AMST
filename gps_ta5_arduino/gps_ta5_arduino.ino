aba86c70acf6a2a180a2955d8004047d34f3a56b
hjalban@espol.edu.ec <hjalban@espol.edu.ec>
2019-12-11 10:25:57 -0500

    gps_code

--- gps_ta5_arduino/gps_ta5_arduino.ino
+++ gps_ta5_arduino/gps_ta5_arduino.ino
@@ -1,61 +1,39 @@
 #include <SoftwareSerial.h>//incluimos SoftwareSerial
 #include <TinyGPS.h>//incluimos TinyGPS
 
- 
-
 TinyGPS gps;//Declaramos el objeto gps
-SoftwareSerial serialgps(11,10);//Declaramos el pin 11 Tx y 10 Rx mega, algunas versiones
+SoftwareSerial serialgps(11,10);//Declaramos el pin 11 Tx y 10 Rx
 
+//Declaramos la variables para la obtención de datos
 unsigned long chars;
 unsigned short sentences, failed_checksum;
 
- 
-
 void setup()
-
 {
-
-
-Serial.begin(115200);//Iniciamos el puerto serie
-
-serialgps.begin(9600);//Iniciamos el puerto serie del gps
-
-//Imprimimos:
-
-Serial.println(" ---Buscando senal--- ");
-
+  Serial.begin(9600);//Iniciamos el puerto serie
+  serialgps.begin(9600);//Iniciamos el puerto serie del gps
+  //Imprimimos:
+  Serial.println("");
+  Serial.println(" ---Buscando senal--- ");
+  Serial.println("");
 }
-
- 
 
 void loop()
-
 {
+  while(serialgps.available()) 
+  {
+    int c = serialgps.read();
+ 
+    if(gps.encode(c))  
+    {
+      float latitude, longitude;
+      gps.f_get_position(&latitude, &longitude);
+      Serial.print("Latitud/Longitud: "); 
+      Serial.print(latitude,5); 
+      Serial.print(", "); 
+      Serial.println(longitude,5);
 
-while(serialgps.available()) 
-
-{
-
-int c = serialgps.read(); 
-
-if(gps.encode(c)) 
-
-{
-
-float latitude, longitude;
-gps.f_get_position(&latitude, &longitude);
-Serial.print("Latitud/Longitud: "); 
-Serial.print(latitude,5); 
-Serial.print(", "); 
-Serial.println(longitude,5);
-Serial.print("\n"); 
-
-Serial.println();
-
-gps.stats(&chars, &sentences, &failed_checksum);
-
-}
-
-}
-
+      gps.stats(&chars, &sentences, &failed_checksum);  
+    }
+  }
 }
